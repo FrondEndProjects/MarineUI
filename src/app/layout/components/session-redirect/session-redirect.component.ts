21cb0caadb021b93d01165e6b711c22a0c72c3ef
Deployment <95078040+MaanWork@users.noreply.github.com>
2025-05-08 19:19:18 +0530

    session direction changed

--- src/app/layout/components/session-redirect/session-redirect.component.ts
+++ src/app/layout/components/session-redirect/session-redirect.component.ts
@@ -15,12 +15,12 @@ export class SessionRedirectComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       // this.router.navigate(['/login'])
-      location.href=`http://192.168.1.48:4900/Eway/#/auth/login`;
+      location.href=`http://197.254.65.234:8080/Eway/#/auth/login`;
 
     },5*60*1000);
   }
   getLogin(){
     // this.router.navigate(['/login'])
-    location.href=`http://192.168.1.48:4900/Eway/#/auth/login`;
+    location.href=`http://197.254.65.234:8080/Eway/#/auth/login`;
   }
 }
